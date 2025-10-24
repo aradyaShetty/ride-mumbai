428d426db6cb0155b4f84a2f18feb02b4cffe36a
Qayam Shaikh <skqayaam01@gmail.com>
2025-10-24 15:19:20 +0530

    project generated  from https://start.spring.io/

---
+++ src/main/java/com/ridemumbai/ridemumbai_api/RidemumbaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.ridemumbai.ridemumbai_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidemumbaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidemumbaiApiApplication.class, args);
+	}
+
+}
